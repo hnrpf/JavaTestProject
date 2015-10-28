1c8b269d98e5f9f2765012b1692459542f4fe109
hnrpf <fu.wei.2000@163.com>
2015-10-28 16:09:08 +0800

    自定义注解Demo

---
+++ JavaProjectTest/src/annotation/FruitColor.java
@@ -0,0 +1,17 @@
+package annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FruitColor {
+
+	public enum Color{ BULE,RED,GREEN};
+	
+	Color fruitColor() default Color.GREEN;
+}
---
+++ JavaProjectTest/src/annotation/TestMain.java
@@ -0,0 +1,9 @@
+package annotation;
+
+public class TestMain {
+	 public static void main(String[] args) {
+	        
+	        FruitInfoUtil.getFruitInfo(Apple.class);
+	        
+	    }
+}
