8cfd1d492c3620c1befb38961e7c4a10b687c99c
hnrpf <fu.wei.2000@163.com>
2015-11-26 14:27:31 +0800

    List链表，subList后的子链表clear后，父链表也被清空了一部分

---
+++ JavaProjectTest/src/SubListTest.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SubListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		System.out.println("before size: " + list.size());
+		List<String> temp = list.subList(0, 2);
+		temp.clear();
+		System.out.println("after size: " + list.size());
+	}
+	
+
+}
